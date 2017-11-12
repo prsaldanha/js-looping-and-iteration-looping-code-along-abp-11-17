f5957c34cad12ee129a96ec91094722add35f614
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 20:17:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const names = ['teddy bear', 'drone', 'doll'];
- 
+
 function wrapGifts (names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    console.log(`Wrapped ${names[i]} and added a bow!`);
   }
- 
-  return gifts;
+
+  return names;
 }
