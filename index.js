a8c8d749b17f0cf013fd529159afc3e38dc0f0cc
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 20:42:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ['teddy bear', 'drone', 'doll'];
+
+function printBadges (items) {
+  for (let i = 0; i < items.length; i++) {
+    console.log(`Welcome ${items[i]}! You are employee #${i+1}.`);
+  }
+
+  return items;
+}
+
+function tailsNeverFails()
+{
+ let counter = 0;
+  while (Math.random() >= 0.5)
+  {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`
+}
