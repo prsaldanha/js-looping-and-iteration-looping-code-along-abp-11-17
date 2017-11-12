7177743e14d2740cb01980277e6fe6e380235580
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 20:17:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const names = ['teddy bear', 'drone', 'doll'];
  
 function wrapGifts (names) {
-  for (let i = 0; i < gifts.length; i++) {
+  for (let i = 0; i < names.length; i++) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
   }
  
