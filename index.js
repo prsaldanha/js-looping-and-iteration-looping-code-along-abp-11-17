978dc5ccc7c6924eb4ca3b7307e24ec331a2e357
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 20:15:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(names)
 {
-  //for (int i=0; i < names.length; i++)
+  for (int i=0; i < names.length; i++)
   console.log(names[i]);
 }
 //return names;
