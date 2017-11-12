7aa162874b85ffaa0900613c32c587ebef228991
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 20:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+var names = ['prem', 'rachel', 'caleb'];
+function printBadges(names)
+{
+   for (let i = 0; i < names.length; i++) {
+     console.log($names[i]);
+   }
+}
+
+function tailsNeverFails
