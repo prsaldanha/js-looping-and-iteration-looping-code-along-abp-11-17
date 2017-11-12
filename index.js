28358327266ae41c2e01819ee9159ad4582015f6
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 20:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const names = ['teddy bear', 'drone', 'doll'];
 
 function printBadges (items) {
   for (let i = 0; i < items.length; i++) {
-    console.log('Welcome ${items[i]}! You are employee #${i+1}.');
+    console.log(`Welcome ${items[i]}! You are employee #${i+1}.`);
   }
 
   return items;
